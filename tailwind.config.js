12c2c514bf1f6330ad5198778ffc0a913a230237
Lyle Liao <lyle@twjoin.com>
2024-10-15 22:31:48 +0800

    feat: add local font jf-openhuninn-2.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "jf-open-huninn": ["jf-open-huninn", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
